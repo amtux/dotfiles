ba5c3c7e20eb793e80a1c8c6ad3353e1dbca93fb
Amandeep Uppal <auppal@fastly.com>
2022-11-01 10:36:07 -0400

    neovim 0.8 setup

---
+++ nvim/lua/user/lsp/groovy.lua
@@ -0,0 +1,5 @@
+ require'lspconfig'.groovyls.setup{
+    -- Unix
+    cmd = { "java", "-jar", "/Users/amtux/go/src/github.com/random/groovy-language-server/build/libs/groovy-language-server-all.jar" },
+    ...
+}
